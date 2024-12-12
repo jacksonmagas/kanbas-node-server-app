e5388467a67ab8ad49ff8b563d787c6c7d6ee52e
Jackson <jacksonmagas@gmail.com>
2024-12-11 20:36:42 -0500

    remove logging

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -46,7 +46,6 @@ export default function UserRoutes(app) {
     console.log(currentUser);
     if (currentUser) {
       req.session["currentUser"] = currentUser;
-      console.log("Sending user " + currentUser + " for session " + req.session);
       res.json(currentUser);
     } else {
       res.status(401).json({ message: "Unable to login. Try again later." });
